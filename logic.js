5d55868bdbf7228bbcfa78a49802f2287ca3ff97
Philip M <philipwaltermay@gmail.com>
2019-03-25 14:05:20 -0500

    add magnitude color and sizing, plus legend

--- logic.js
+++ logic.js
@@ -1,6 +1,6 @@
 // initial map attributes
-var Coords = [10, 10];
-var mapZoomLevel = 3;
+var Coords = [40, -100];
+var mapZoomLevel = 4;
 
 // initializes map
 var myMap = L.map("map-id", {
@@ -19,6 +19,22 @@ L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={
 // url link to USGS earthquake data (all month) 
 var url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_month.geojson";
 
+// Function to determine circle size based on magnitude
+function circleSize(m) {
+    return m * 13000;
+  }
+
+// Function to determine circle color based on magnitude
+function circleColor(m2) {
+    return m2 > 5 ? '#db1200' :
+           m2 > 4  ? '#db5300' :
+           m2 > 3  ? '#db9200' :
+           m2 > 2  ? '#dbcc00' :
+           m2 > 1   ? '#c1db00' :
+                      '#57db00';
+}
+
+
 // Get request to query the url
 d3.json(url, function(response) {
     var earthquakeFeatures = response.features;
@@ -28,10 +44,36 @@ d3.json(url, function(response) {
     // Loop through the array
     for (var i = 0; i < earthquakeFeatures.length; i++) {  
     // For each event, create a marker and bind a popup with the name
+        var mag = earthquakeFeatures[i].properties.mag;
         earthquakeMarkers.push(
-        L.marker([earthquakeFeatures[i].geometry.coordinates[1], earthquakeFeatures[i].geometry.coordinates[0]]).bindPopup("<h1>" + "Location: " + earthquakeFeatures[i].properties.place + "<br>" + "Magnitude: " + earthquakeFeatures[i].properties.mag + "</h1>").addTo(myMap)
+        L.circle([earthquakeFeatures[i].geometry.coordinates[1], earthquakeFeatures[i].geometry.coordinates[0]],{
+            color: 'black',
+            fillColor: circleColor(mag),
+            weight: 1,
+            opacity: 1,
+            fillOpacity: .7,
+            radius: circleSize(mag)
+        })
+        .bindPopup("<h1>" + "Location: " + earthquakeFeatures[i].properties.place + "<br>" + "Magnitude: " + mag + "</h1>").addTo(myMap)
         );
     }
 })
 
+var legend = L.control({position: 'bottomright'});
 
+legend.onAdd = function (myMap) {
+
+    var div = L.DomUtil.create('div', 'info legend'),
+        magnitude = [0, 1, 2, 3, 4, 5],
+        labels = [];
+
+    for (var i = 0; i < magnitude.length; i++) {
+        div.innerHTML +=
+            '<i style="background:' + circleColor(magnitude[i] + 1) + '"></i> ' +
+            magnitude[i] + (magnitude[i + 1] ? '&ndash;' + magnitude[i + 1] + '<br>' : '+');
+    }
+
+    return div;
+};
+
+legend.addTo(myMap);
