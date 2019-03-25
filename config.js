40f0ebb1ee1fc03a8851c96f720a2e66556d5ea2
Philip M <philipwaltermay@gmail.com>
2019-03-25 09:13:38 -0500

    fix coordinates

---
+++ config.js
@@ -0,0 +1,2 @@
+// API key
+var API_KEY = "pk.eyJ1IjoicG1heTE5IiwiYSI6ImNqdGtzN2JtYjE0Ymk0NHF3dnNic2lxa2gifQ.5KvPQKzT2is5rkG_a4CySQ";
--- logic.js
+++ logic.js
@@ -10,11 +10,11 @@ var myMap = L.map("map-id", {
 
 // Create the tile layer that will be the background of our map
 L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
-  attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
-  maxZoom: mapZoomLevel,
-  id: "mapbox.streets",
-  accessToken: API_KEY
-  }).addTo(myMap);
+    attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+    maxZoom: mapZoomLevel,
+    id: "mapbox.streets",
+    accessToken: API_KEY
+    }).addTo(myMap);
 
 // url link to USGS earthquake data (all month) 
 var url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_month.geojson";
@@ -29,7 +29,7 @@ d3.json(url, function(response) {
     for (var i = 0; i < earthquakeFeatures.length; i++) {  
     // For each event, create a marker and bind a popup with the name
         earthquakeMarkers.push(
-        L.marker([earthquakeFeatures[i].geometry.coordinates[0], earthquakeFeatures[i].geometry.coordinates[1]]).bindPopup("<h1>" + "Location: " + earthquakeFeatures[i].properties.place + "<br>" + "Magnitude: " + earthquakeFeatures[i].properties.mag + "</h1>").addTo(myMap)
+        L.marker([earthquakeFeatures[i].geometry.coordinates[1], earthquakeFeatures[i].geometry.coordinates[0]]).bindPopup("<h1>" + "Location: " + earthquakeFeatures[i].properties.place + "<br>" + "Magnitude: " + earthquakeFeatures[i].properties.mag + "</h1>").addTo(myMap)
         );
     }
 })
